db4dc5f05beee93f0509a12a40b7520c1440105b
Abraham Boakye <86990808+Laudkyle@users.noreply.github.com>
2024-11-24 06:02:07 +0000

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto"); // For generating unique IDs
 
 const orderSchema = new mongoose.Schema({
+  orderId: { type: String, unique: true }, // Unique order ID
   driverId: { type: mongoose.Schema.Types.ObjectId, ref: "Driver", required: true },
   products: [
     {
@@ -12,6 +14,14 @@ const orderSchema = new mongoose.Schema({
   dateCreated: { type: Date, default: Date.now },
   dateModified: { type: Date },
   additionalInfo: { type: String },
+});
+
+// Pre-save hook to generate unique orderId
+orderSchema.pre("save", function (next) {
+  if (!this.orderId) {
+    this.orderId = crypto.randomBytes(8).toString("hex"); 
+  }
+  next();
 });
 
 module.exports = mongoose.model("Order", orderSchema);
