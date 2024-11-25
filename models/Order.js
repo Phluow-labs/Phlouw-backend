9d432c99d37289940468045701aba38a5cc86a2f
Laudkyle <kyleaby1@gmail.com>
2024-11-25 13:16:09 +0000

    latest encryption changes

--- models/Order.js
+++ models/Order.js
@@ -3,15 +3,23 @@ const crypto = require("crypto"); // For generating unique IDs
 
 const orderSchema = new mongoose.Schema({
   orderId: { type: String, unique: true }, // Unique order ID
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  driverId: { type: mongoose.Schema.Types.ObjectId, ref: "Driver", required: true },
+  userId: { type: String, required: true },
+  driverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Driver",
+    required: true,
+  },
   products: [
     {
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
       number: { type: Number, required: true },
     },
   ],
-  totalPrice:{ type: Number, required: true },
+  totalPrice: { type: Number, required: true },
   deliveryStatus: { type: String, default: "Pending" },
   dateCreated: { type: Date, default: Date.now },
   dateModified: { type: Date },
@@ -21,7 +29,7 @@ const orderSchema = new mongoose.Schema({
 // Pre-save hook to generate unique orderId
 orderSchema.pre("save", function (next) {
   if (!this.orderId) {
-    this.orderId = crypto.randomBytes(8).toString("hex"); 
+    this.orderId = crypto.randomBytes(8).toString("hex");
   }
   next();
 });
