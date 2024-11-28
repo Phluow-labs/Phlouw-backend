ad4ec5ececb2cf794b6e70674e5abe92f7f85e66
Laudkyle <kyleaby1@gmail.com>
2024-11-28 11:48:16 +0000

    porting

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
-// Start Server
+  // Creating the port 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
