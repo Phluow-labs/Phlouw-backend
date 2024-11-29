525230b5db10c8f22b29e2ce74904600979d44f5
Laudkyle <kyleaby1@gmail.com>
2024-11-29 03:48:41 +0000

    Adding code to driver

--- models/Driver.js
+++ models/Driver.js
@@ -7,6 +7,7 @@ const driverSchema = new mongoose.Schema({
   carNumber: { type: String, required: true },
   region: { type: String, required: true },
   location:{ type: String, required: true },
+  code:{ type: String, required: true },
 });
 
 module.exports = mongoose.model("Driver", driverSchema);
