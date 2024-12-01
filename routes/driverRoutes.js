a8d423eade243d66d1a307155c2caf77d21fa803
Laudkyle <kyleaby1@gmail.com>
2024-12-01 04:25:23 +0000

    removing the case sensitive nature of fullname

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -59,18 +59,26 @@ router.delete("/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+
 router.post("/login", async (req, res) => {
   const { fullName, code } = req.body;
 
   try {
-    const driver = await Driver.findOne({ fullName, code });
+    const driver = await Driver.findOne({
+      fullName: { $regex: new RegExp(`^${fullName}$`, "i") }, 
+      code,
+    });
+
     if (!driver) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
+
     res.status(200).json(driver);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
+
 
 module.exports = router;
